bbcdc81485602f483d8735554ad9434413f1ccea
yvonne <calmmidnigh@gmail.com>
2026-05-26 09:09:04 +0900

    Fix: Clarity 스크립트 id 충돌로 인한 행동 감지 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         {children}
         <FloatingChat />
-        <Script id="clarity" strategy="afterInteractive">
+        <Script id="microsoft-clarity" strategy="afterInteractive">
           {`(function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
